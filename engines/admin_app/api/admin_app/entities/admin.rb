60a47c3a279cd084de723b0c258f970efaed09c0
JustSnow <justsnow17@yandex.ru>
2015-12-23 16:26:01 +0300

    feature Add entities for admins

---
+++ engines/admin_app/api/admin_app/entities/admin.rb
@@ -0,0 +1,17 @@
+module AdminApp
+  module Entities
+    class Admin < Grape::Entity
+      root :admins, :admin
+
+      expose :id
+      expose :email
+      expose :first_name
+      expose :last_name
+      expose :full_name
+      expose :role
+
+      expose :created_at
+      expose :updated_at
+    end
+  end
+end
--- engines/admin_app/api/admin_app/versions/v10/admins.rb
+++ engines/admin_app/api/admin_app/versions/v10/admins.rb
@@ -1,21 +1,50 @@
 module AdminApp
   module Versions
     class V10::Admins < Grape::API
-      resource :admins do
-        desc 'List admins'
-        get do
+      helpers do
+        params :pagination do
+          optional :page, type: Integer
         end
 
-        desc 'Edit admin'
-        get ':id/edit' do
+        # TODO add config for store settings
+        def admins
+          @admins ||= Admin.all.paginate page: params[:page], per_page: 1
+        end
+
+        def admin
+          @admin ||= Admin.find params[:id]
+        end
+      end
+
+      resource :admins do
+        desc 'List admins'
+        params do
+          use :pagination
+        end
+
+        get do
+          present :total_pages, admins.total_pages
+          present :next_page?, admins.next_page.present?
+          present admins, with: Entities::Admin
         end
 
         desc 'Create admin'
         post do
         end
 
-        desc 'Update admin'
-        put :id do
+        route_param :id do
+          desc 'Show admin'
+          get do
+            present admin, with: Entities::Admin
+          end
+
+          desc 'Update admin'
+          put do
+          end
+
+          desc 'Delete admin'
+          delete do
+          end
         end
       end
     end
